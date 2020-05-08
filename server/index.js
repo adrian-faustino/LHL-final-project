247fa9c739aadef07f3917de0e0317c162b99b99
Adrian Faustino <adrianfaustino@Adrians-MacBook-Pro.local>
2020-05-08 11:17:30 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const PORT = process.env.PORT || 5555
+
+//middleware
+//app.use(cors())  --> uncomment when we need
+
+//basic route
+app.get('/test', (req, res) => {
+  res.json({message: "Hello World"})
+})
+
+//server
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API = "http://localhost:5555/test"
+
 function App() {
+
+  fetch(API)
+    .then(data => data.json())
+    .then(result => console.log(result))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hey it works again
     </div>
   );
 }
