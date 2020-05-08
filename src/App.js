f4e5581088c95f45fd22bfa96881f3e4cc7e56cf
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-08 14:30:35 -0400

    set up views component folder

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 
 // View Components
-import DrawGameView from './components/DrawGameView';
-import GuestLobbyView from './components/GuestLobbyView';
-import HostLobbyView from './components/HostLobbyView';
-import InstructionsView from './components/InstructionsView';
-import LandingView from './components/LandingView';
-import ResultsView from './components/ResultsView';
-import ShareView from './components/ShareView';
+import DrawGameView from './components/views/DrawGameView';
+import GuestLobbyView from './components/views/GuestLobbyView';
+import HostLobbyView from './components/views/HostLobbyView';
+import InstructionsView from './components/views/InstructionsView';
+import LandingView from './components/views/LandingView';
+import ResultsView from './components/views/ResultsView';
+import ShareView from './components/views/ShareView';
 
 function App() {
   const API = "http://localhost:5555/test"
+  const [view, setView] = setSate('');
 
 
   return (
