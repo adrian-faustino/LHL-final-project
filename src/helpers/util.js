28c212f906f4e8a784d1cff7074ef08d99edb77e
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-08 19:16:32 -0400

    added helper function for generating random room id

---
+++ src/helpers/util.js
@@ -0,0 +1,28 @@
+// returns a random integer inclusize of min and max
+const randomNumAllInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// returns a 6 digit alphanumeric all capital ID
+const generateRoomID = (ID_length) => {
+  // List does not include 0 or O;
+  const ALPHA_NUM = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
+  let id = ''
+
+  // push random letters/numbers into the array until desired ID length
+  for (let i = 0; i < ID_length; i++) {
+    const randomIndex = randomNumAllInclusive(0, ALPHA_NUM.length - 1);
+    id += ALPHA_NUM[randomIndex]
+  }
+
+  return id;
+}
+
+const util = {
+  randomNumAllInclusive,
+  generateRoomID
+};
+
+export default util;
