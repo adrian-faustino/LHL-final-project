172886af71c6c419b7e2e0473d1a9c96ef9022ad
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-10 00:57:03 -0400

    set up crud functions

--- server/routes/handleCRUD.js
+++ server/routes/handleCRUD.js
@@ -4,11 +4,80 @@ module.exports = function(client, db) {
 
   // ==========> CREATE
 
+  /* Add lobby to DB */
+  client.on('createLobby', data => {
+    const { lobbyID } = data;
+
+    const newLobby = new Lobby({
+      lobbyID,
+      players: []
+    })
+
+    newLobby.save().then(() => {
+      console.log(`Successfully added ${lobbyID} to the DB.`);
+      client.emit('success', `Successfully added ${lobbyID} to the DB.`);
+    }).catch(err => {
+      console.log(err);
+      client.emit('err', err);
+    });
+  })
+
+
+  /* Add user to DB */
+  client.on('createPlayer', data => {
+    const { username, coordinates } = data;
+
+    const newPlayer = new Player({
+      username,
+      coordinates
+    });
+
+    newPlayer.save().then(() => {
+      console.log(`Successfully added ${username} to the DB.`);
+      client.emit('success', `Successfully added ${username} to the DB.`);
+    }).catch(err => {
+      console.log(err);
+      client.emit('err', err);
+    });
+  });
+
 
   // ==========> READ
-  /* Given 'lobbyID', return that lobby object
+
+  /* Given 'lobbyID', return that lobby object from the DB */
+  client.on('findLobby', data => {
+    const { lobbyID } = data;
+
+    Lobby.findOne({lobbyID}, (err, lobbyObj) => {
+      if(err) {
+        console.log(`Failed to find lobby: ${err}`);
+        client.emit('err', err);
+      } else {
+        console.log(`Successfully found lobby: ${lobbyID}`);
+        client.emit('gameState', lobbyObj);
+      }
+    });
+  });
+
 
   // ==========> UPDATE
+  
+  /* Given 'lobbyID', add player to players array (players in lobby) */
+  client.on('addToPlayers', data => {
+    const { lobbyID, players } = data;
+    const filter = { lobbyID };
+    const update = { players };
+
+    Lobby.findOneAndUpdate(filter, update, { new: true }, (err, lobbyObj) => {
+      if(err) {
+        console.log(err);
+        client.emit('err', err);
+      } else {
+        console.log(`Successfully updated game state.`);
+        client.emit('gameState', lobbyObj);
+      }
+    });
+  })
 
   // ==========> DELETE
 
