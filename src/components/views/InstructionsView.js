84902b10f28d5d1688b59e409415d46019b48bc9
Jason Melnikel <jmelnikel@gmail.com>
2020-05-12 16:39:13 -0400

    added styling to InstructionsView

--- src/components/views/InstructionsView.js
+++ src/components/views/InstructionsView.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react'
+import "./InstructionsView.css";
+import MLforInstruct from './MLforInstruct.jpg';
 
 // subcomponents
 import NavButton from '../NavButton'
@@ -31,12 +33,18 @@ export default function InstructionsView(props) {
 
   return (
     <div>
-      <h1 style={{color: "red", fontSize: "14px"}}>InstructionsView.js</h1>
-
-      <h3 style={{color: "red", fontSize: "14px"}}>Your goal is to draw a picture as a team!</h3>
-
-      <p style={{color: "red", fontSize: "14px"}}>You will draw this section of the image. You will have 3 minutes to draw!</p>
+      {/* <h1 style={{color: "red", fontSize: "14px"}}>InstructionsView.js</h1> */}
+      <div className="InstructionsView__header">
+        <p>1:16</p>
+        <button><i class="fas fa-palette"></i></button>
+      </div>
       
+      <div className="InstructionsView__image--toDraw">
+        <img className="InstructionsView__image--reference" src={MLforInstruct} alt="Portion of image to draw."></img>
+      
+      </div>
+
+
       {/* <MainImage /> */}
 
       {/* {state.playerAmt && <span>Players required to skip: {state.playerAmt}</span>}
@@ -45,6 +53,13 @@ export default function InstructionsView(props) {
       nextView={'DrawGameView'}
       buttonTitle={'Skip'}
       changeViewHandler={props.changeViewHandler}/> */}
+
+      
     </div>
   )
 }
+
+
+// <h3 style={{color: "red", fontSize: "14px"}}>Your goal is to draw a picture as a team!</h3>
+// <p style={{color: "red", fontSize: "14px"}}>You will draw this section of the image. You will have 3 minutes to draw!</p>
+      
