18c3345401f6a213d005de9ce70d455815d58047
Jason Melnikel <jmelnikel@gmail.com>
2020-05-13 14:43:19 -0400

    Styled ResutlsView

--- src/components/views/ResultsView.js
+++ src/components/views/ResultsView.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import './ResultsView.css'
+
+// Begin: Dummy code to have static reference image. Take out when real image is available.
+import MLReference from '../../assets/MLReference.jpg';
+// End: Dummy code to have static reference image. Take out when real image is available.
+
 
 // Notes: We will remove the NavButton component later and replace it with a socket emit so it automatically moves to the next page for all the players
 
@@ -7,13 +13,23 @@ import NavButton from '../NavButton'
 
 export default function ResultsView(props) {
   return (
-    <div>
+    <div className="scrolling-background">
       <h1 style={{color: "red", fontSize: "14px"}}>ResultsView.js</h1>
 
-      <NavButton
-      nextView={'ShareView'}
-      buttonTitle={'Done'}
-      changeViewHandler={props.changeViewHandler}/>
+      <h1 className="ResultsView__container--title">Ta-Daaa!</h1>
+
+      <h2 className="ResultsView__message">Such a work of art!</h2>
+        
+      <img className="ResultsView__image" src={MLReference} alt="Portion of image to draw."></img>
+
+      {/* I had to create a div to contain this to be able to control it. */}
+      <div className="button-temp">
+        <NavButton
+        nextView={'ShareView'}
+        buttonTitle={'Done'}
+        changeViewHandler={props.changeViewHandler}
+        />
+      </div>
     </div>
   )
 }
