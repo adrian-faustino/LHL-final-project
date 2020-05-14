94ec281cf2432b0d8724141d87ac6a9cd0f1f9b9
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-14 03:30:05 -0400

    merging

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // assets
 import IMG_SRC from '../assets/mona-lisa.jpg'
@@ -7,8 +7,18 @@ import IMG_SRC from '../assets/mona-lisa.jpg'
 import './MainImage.css';
 import MainImageStyles from './MainImageStyles';
 
+const { imgStyles,
+  imgContainerStyles,
+  containerWrapperStyles } = MainImageStyles;
+  
+
 export default function MainImage(props) {
   const { myQuadrant } = props;
+
+  const [state, setState] = useState({
+    isClicked: false
+  });
+  const { isClicked } = state;
 
   // constants
   const IMG_CONTAINER_WIDTH = window.innerWidth * 0.7;
@@ -16,18 +26,25 @@ export default function MainImage(props) {
   const IMG_WIDTH = IMG_CONTAINER_HEIGHT * 2;
   const IMG_HEIGHT = IMG_CONTAINER_HEIGHT * 2;
 
-  // styles
-  const { imgStyles,
-    imgContainerStyles,
-    containerWrapperStyles } = MainImageStyles;
+  /*** make shallow copies and modify - fix for strict mode? ***/
+  const _imgStyles = imgStyles();
+  const _imgContainerStyles = imgContainerStyles();
+  const _containerWrapperStyles = containerWrapperStyles();
+
+  const onClickHandler = () => {
+    setState(prev => ({...prev, isClicked: !isClicked}));
+  }
+
+  if(isClicked) {
+    _imgStyles['height'] = '300px';
+  }
 
   return (
-    <div className="MainImage__container-wrapper">
-      <div className="MainImage__image-container"width={IMG_CONTAINER_WIDTH}
-      height={IMG_CONTAINER_HEIGHT}>
+    <div style={_containerWrapperStyles}>
+      <div style={_imgContainerStyles}>
         <img
-        className="MainImage__image"
-        style={imgStyles}
+        onClick={onClickHandler}
+        style={_imgStyles}
         src={IMG_SRC}/>
       </div>
     </div>
--- src/components/MainImageStyles.js
+++ src/components/MainImageStyles.js
@@ -1,15 +1,47 @@
-const imgStyles = {
-  height: '50px',
-  width: '50px'
-};
+const imgStyles = (key, val) => {
+  const style = {
+    position: 'relative',
+    bottom: '400px',
+    right: '300px',
+    height: '800px',
+    width: '600px'
+  }
+  
+  if(key) {
+    style[key] = val;
+  }
+  return style;
+}
 
-const imgContainerStyles = {
+const imgContainerStyles = (key, val) => {
+  const style = {
+    height: '400px',
+    width: '300px',
+    overflow: 'hidden'
+  }
+  
+  if(key) {
+    style[key] = val;
+  }
 
-};
+  return style;
+}
 
-const containerWrapperStyles = {
+const containerWrapperStyles = (key, val) => {
+  const style = {
+    height: '100vh',
+    width: '100vw',
+    display: 'grid',
+    'justify-content': 'center',
+    'align-items': 'center',
+  }
+  
+  if(key) {
+    style[key] = val;
+  }
 
-};
+  return style;
+}
 
 const MainImageStyles = {
   imgStyles,
