3bcbfc4ec7724d62fe229af8ff786976e105da62
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-16 18:33:52 -0400

    changed game time

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /** All time variables in milliseconds **/
 
-const VIEW_TIME = 10000; // for InstructionsView;
-const ROUND_TIME = 10000;
+const VIEW_TIME = 1000; // for InstructionsView;
+const ROUND_TIME = 3000000;
 const API = 'http://localhost:5555';
 const MIN_RENDER_TIME = 5000; // for rendering final picture
 
