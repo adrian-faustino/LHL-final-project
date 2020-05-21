3d6c40a8af2fcf5835355a3570a7ff1fe30993c6
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-21 13:53:59 -0400

    deploying

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /** All time variables in milliseconds **/
 
 /** Switch between dev mode and production **/
-const isDevMode = true;
+const isDevMode = false;
 const API = isDevMode ? 'http://localhost:5555' : 'https://draw-mageddon.herokuapp.com';
 
 /** length of time in InstructionsView **/
